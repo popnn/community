1b0fd3bcd5c458762b4a7e3001360ad4897f3a3e
samay-1305 <samay13g@gmail.com>
2019-12-21 22:29:48 +0530

    revert

--- community/urls.py
+++ community/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('profile/', views.profilepage, name='community-profile'),
     path('profile/edit/', views.editprofilepage, name='community-profile-edit'),
     path('discussions/', views.alldiscussionspage, name='community-discussions'),
-    path('discussions/<str:username>/new', views.newdiscussionpage, name='community-discussions-new'),
-    path('discussions/<str:username>', views.mydiscussionpage, name='community-discussions-user'),
-    path('discussions/<str:username>/<int:discussion_id>', views.selectdiscussionpage, name='community-discussions-single'),
+    path('discussions/<slug:username>/new', views.newdiscussionpage, name='community-discussions-new'),
+    path('discussions/<slug:username>', views.mydiscussionpage, name='community-discussions-user'),
+    path('discussions/<slug:username>/<int:discussion_id>', views.selectdiscussionpage, name='community-discussions-single'),
 ]
