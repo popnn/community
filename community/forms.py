3b74df49f60f9a8b9aa087822cb702cc39fc082d
arham musheer <arhammusheer@gmail.com>
2019-12-21 17:37:08 +0530

    new form stuff

--- community/forms.py
+++ community/forms.py
@@ -7,8 +7,8 @@ class CreateUserForm(forms.Form):
     pass
 
 class EditProfileForm(forms.Form):
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=30)
-    email = forms.EmailField(max_length=256)
-    description = forms.CharField(max_length=256, required=False)
-    profile_image = forms.ImageField(required=False)
+    first_name = forms.CharField(max_length=30,widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(max_length=30,widget=forms.TextInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(max_length=256,widget=forms.TextInput(attrs={'class':'form-control'}))
+    description = forms.CharField(max_length=256, required=False,widget=forms.TextInput(attrs={'class':'form-control'}))
+    profile_image = forms.ImageField(required=False,widget=forms.FileInput(attrs={'class':'form-control'}))
