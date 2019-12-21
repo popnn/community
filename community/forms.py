6980cd81eb74be26aebd9b6165c5b96f47c53c22
samay-1305 <samay13g@gmail.com>
2019-12-22 01:23:54 +0530

    form

--- community/forms.py
+++ community/forms.py
@@ -19,6 +19,6 @@ class CreateUserForm(forms.Form):
 class EditProfileForm(forms.Form):
     first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(max_length=256, widget=forms.TextInput(attrs={'class': 'form-control'}), disabled=True)
+    email = forms.EmailField(max_length=256, widget=forms.TextInput(attrs={'class': 'form-control'}),)
     description = forms.CharField(max_length=256, required=False, widget=forms.Textarea(attrs={'class': 'form-control'}))
     profile_image = forms.ImageField(required=False, widget=forms.FileInput(attrs={'class': 'form-control'}))
