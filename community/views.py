bae1f9f57aaa5b16a85c468c50585b568a233949
samay-1305 <samay13g@gmail.com>
2019-12-24 03:13:12 +0530

    #

--- community/views.py
+++ community/views.py
@@ -30,6 +30,7 @@ def render_template(request, template_name, context={}):
 
 @csrf_exempt
 def ajax_response(request):
+    print("#"*10)
     if request.method=="POST":
         conv_id = int(request.POST.get('conversation_id'))
         outbox = []
