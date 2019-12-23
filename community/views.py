bc44c09c16ea0ee10192e9b19324b0aa4cb41cf4
samay-1305 <samay13g@gmail.com>
2019-12-24 03:24:26 +0530

    oops

--- community/views.py
+++ community/views.py
@@ -34,13 +34,12 @@ def ajax_response(request):
         conv_id = int(request.POST.get('conversation_id'))
         outbox = []
         for msg_id in Conversations.objects.get(conversation_id=int(conv_id)).conversation_history.split(","):
-            if msg_id != ''
+            if msg_id != '':
                 msg = ConversationMessages.objects.get(message_id=int(msg_id))
                 time_dif = (datetime.datetime.now(datetime.timezone.utc) - msg.message_time).seconds 
                 if time_dif < 3 and request.COOKIES.get('id', None) != msg.user_id:
                     line = "<p><strong>{}:</strong>{}</p>".format(UserProfiles.objects.get(user_id=msg.user_id).username, msg.message_text)
                     outbox.append(line)
-
         result = {"new_data":outbox}
         response = JsonResponse(result)
         response.set_cookie('load', datetime.datetime.now().strftime("%H:%M:%S.%f %b %d %Y"))
