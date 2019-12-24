fe9cbfe6d98881defb7159c454c80831c5637ea4
arham musheer <arhammusheer@gmail.com>
2019-12-25 00:31:35 +0530

    i forgot this

--- community/views.py
+++ community/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
