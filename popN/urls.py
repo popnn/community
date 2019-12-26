fc850641a04f47c2b6883b0371a9bdd9b6c25d1f
arham musheer <arhammusheer@gmail.com>
2019-12-27 02:46:04 +0530

    password reset

--- popN/urls.py
+++ popN/urls.py
@@ -34,7 +34,30 @@ urlpatterns = [
     path('logout/', logout_function, name='logout'),
     path('', include('community.urls'), name='community'),
     path('', include('pwa_webpush.urls')),
-    path('', include('django.contrib.auth.urls'))
+    #password Reset
+    path('reset-password/',
+         auth_views.PasswordResetView.as_view(
+            template_name = 'passReset/password_reset_form.html',
+            subject_template_name='passReset/password_reset_subject.txt',
+            email_template_name='passReset/password_reset_email.html',
+            success_url='/login/'
+        ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='passReset/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='passReset/password_reset_confirm.html'
+         ),
+        name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='passReset/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
