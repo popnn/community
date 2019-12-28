1ae4dc7dd724c906de3ba2af1f06b091add59eab
arham musheer <arhammusheer@gmail.com>
2019-12-29 04:00:48 +0530

    redis add

--- popN/settings.py
+++ popN/settings.py
@@ -128,6 +128,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
