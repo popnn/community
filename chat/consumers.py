c72584f7200be2dba4f2913f92cde46be8deb18a
arham musheer <arhammusheer@gmail.com>
2019-12-29 08:30:54 +0530

    new

--- chat/consumers.py
+++ chat/consumers.py
@@ -28,7 +28,7 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
+        add_log(message)
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
